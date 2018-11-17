6482aec1ae6148205f4a8221485604ba701ffc3d
Arun Chandrasekaran <achandrasekaran@wfscorp.com>
2018-11-16 22:38:06 -0500

    Initial Commit

---
+++ src/main/java/org/arun/springoauth/Startup.java
@@ -0,0 +1,12 @@
+package org.arun.springoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Startup.class, args);
+  }
+}
